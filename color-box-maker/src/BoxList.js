c889532a25694b445a7b0f4937834b57e64ebfbc
Agnes <agnes.mccall5@gmail.com>
2024-08-22 20:57:47 -0400

    add NewBoxForm

--- color-box-maker/src/BoxList.js
+++ color-box-maker/src/BoxList.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import Box from './Box';
+import NewBoxForm from './NewBoxForm';
+import { v4 as uuidv4 } from 'uuid';
 
 const BoxList = () => {
     const [boxes, setBoxes] = useState([{color: "red", width: 100, height: 100}]);
+
+    const addBox = (newBox) => {
+        console.log(newBox)
+        setBoxes(boxes => [...boxes, newBox])
+    }
+
     return (
         <div>
             <h1>Box List</h1>
             {boxes.map((box) => 
-                <Box key={box.id} {...box} />)}
+                <Box key={uuidv4()} {...box} />)}
+            <NewBoxForm addBox={addBox}/>
         </div>
     )
 }
---
+++ color-box-maker/src/NewBoxForm.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const NewBoxForm = ({addBox}) => {
+    const INITIAL_STATE = {
+        width: "",
+        height: "",
+        color: ""
+    }
+    const [formData, setFormData] = React.useState(INITIAL_STATE)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData)
+        addBox({...formData})
+        setFormData(INITIAL_STATE)
+    }   
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setFormData(formData => ({
+            ...formData,
+            [name]: value
+        }))
+    }   
+
+    return (
+        <div>   
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="width">Width</label>
+                    <input id="width" name="width" type="text" 
+                        value={formData.width} 
+                        onChange={handleChange}/>
+                </div>  
+                <div>   
+                    <label htmlFor="height">Height</label>
+                    <input id="height" name="height" type="text" 
+                        value={formData.height}
+                        onChange={handleChange}/>
+                </div>
+                <div>
+                    <label htmlFor="color">Color</label>
+                    <input id="color" name="color" type="text" 
+                        value={formData.color}
+                        onChange={handleChange}/>
+                </div>
+                <button>Add a new box</button>
+            </form>
+        </div>
+    )
+}   
+
+export default NewBoxForm;
